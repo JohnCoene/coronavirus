dbd2fb8f732ed5b4c5d183b0fd85e9d307f1ca94
JohnCoene <jcoenep@gmail.com>
2020-02-04 12:29:56 +0100

    worker scope

--- inst/app/www/service-worker.js
+++ inst/app/www/service-worker.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('./www/sw.js', {scope: '/'}).then(function(registration) {
+  navigator.serviceWorker.register('./www/sw.js', {scope: '/coronavirus'}).then(function(registration) {
     console.log('ServiceWorker registration successful with scope:',  registration.scope);
   }).catch(function(error) {
     console.log('ServiceWorker registration failed:', error);
