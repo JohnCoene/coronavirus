11e0099ec117fd64441812f553b1975626ea1059
JohnCoene <jcoenep@gmail.com>
2020-02-07 18:09:59 +0100

    delete useless files

--- inst/app/www/service-worker.js
+++
@@ -1,7 +0,0 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('./www/sw.js').then(function(registration) {
-    console.log('ServiceWorker registration successful with scope:',  registration.scope);
-  }).catch(function(error) {
-    console.log('ServiceWorker registration failed:', error);
-  });
-}
--- inst/app/www/sw.js
+++
@@ -1,1 +0,0 @@
-console.log('I am a Service Worker!');
