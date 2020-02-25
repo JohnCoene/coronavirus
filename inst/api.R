a82ed5481a77d75745e9710798b449a8d3ec1acb
JohnCoene <jcoenep@gmail.com>
2020-02-25 10:39:27 +0100

    improve API

--- inst/api.R
+++ inst/api.R
@@ -1,15 +1,70 @@
 #* Get John Hopkins Data
+#* @param type Type of cases to return; \code{confirmed}
+#* \code{recovered}, or \code{death}.
+#* @param region Region to return either \code{china}, or \code{other}.
 #* @serializer unboxedJSON
 #* @get /jhu
-function(){
+function(res, type, region){
+
+  msg <- ""
+
+  # check inputs
+  if(missing(type))
+    msg <- "Must specify type, one of confirmed, recovered, death"
+
+  if(missing(region))
+    msg <- "Must specify region, either china, or other"
+
+  if(msg != ""){
+    res$status <- 400
+    return(
+      list(
+        error = jsonlite::unbox(msg)
+      )
+    )
+  }
+
+  if(grepl(";", type))
+    msg <- "No SQL injection please."
+
+  if(!type %in% c("confirmed", "recovered", "death"))
+    msg <- "Invalid type, must be one of confirmed, recovered, death"
+
+  if(!region %in% c("china", "other"))
+    msg <- "Invalid region, must be china, or other"
+
+  if(msg != ""){
+    res$status <- 400
+    return(
+      list(
+        error = jsonlite::unbox(msg)
+      )
+    )
+  }
+
+  # define clause
+  if(region == "china")
+    country <- "country = 'Mainland China'"
+  else
+    country <- "country <> 'Mainland China'"
+
   con <- coronavirus:::connect()
 
   on.exit({
     coronavirus:::disconnect()
   })
 
-  DBI::dbReadTable(con, "jhu") %>% 
-    purrr::transpose()
+  data <- DBI::dbGetQuery(
+    con, 
+    paste0("SELECT * FROM jhu WHERE type = '", type, "' AND ", country, ";")
+  ) 
+  data <- purrr::transpose(data)
+
+  list(
+    source = "John Hopkins CSSE",
+    url = "https://github.com/CSSEGISandData/COVID-19",
+    data = data
+  )
 }
 
 #* Get Weixin Data
@@ -22,11 +77,16 @@ function(){
     coronavirus:::disconnect()
   })
 
-  details <- DBI::dbReadTable(con, "weixin") %>% 
-    purrr::transpose()
+  total <- DBI::dbReadTable(con, "weixin_total")
+  total <- purrr::transpose(total)
+
+  details <- DBI::dbReadTable(con, "weixin") 
+  details <- purrr::transpose(details)
 
   list(
-    total = DBI::dbReadTable(con, "weixin_total"),
+    source = "Tencent QQ",
+    url = "https://github.com/GuangchuangYu/nCov2019",
+    total = total,
     data = details
   )
 }
@@ -41,6 +101,12 @@ function(){
     coronavirus:::disconnect()
   })
 
-  DBI::dbReadTable(con, "dxy") %>% 
-    purrr::transpose()
+  data <- DBI::dbReadTable(con, "dxy")
+  data <- purrr::transpose(data)
+
+  list(
+    source = "DingXiangYing",
+    url = "https://ncov.dxy.cn/ncovh5/view/pneumonia",
+    data = data
+  )
 }
