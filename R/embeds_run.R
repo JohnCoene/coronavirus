65174998718b964e7d589735900e68e0a927b743
JohnCoene <jcoenep@gmail.com>
2020-02-26 21:34:57 +0100

    remove golem options from embeds

--- R/embeds_run.R
+++ R/embeds_run.R
@@ -7,8 +7,5 @@ run_embeds <- function(){
   addResourcePath(
     'www', system.file('app/www', package = 'coronavirus')
   )
-  with_golem_options(
-    app = shinyApp(ui = embeds_ui, server = embeds_server), 
-    golem_opts = list()
-  )
+  shinyApp(ui = embeds_ui, server = embeds_server)
 }
