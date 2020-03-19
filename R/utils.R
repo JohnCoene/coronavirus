42a4d92a1bcb56c8b9f912b5caaa3860974bf00a
JohnCoene <jcoenep@gmail.com>
2020-03-19 16:28:43 +0100

    move echarst4r to Imports fixes #18

--- R/utils.R
+++ R/utils.R
@@ -11,7 +11,7 @@ recovered_sheet <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/ma
 # pallettes for DXY
 deaths_pal <- c("#263238", "#455a64", "#607d8b", "#90a4ae", "#cfd8dc") %>% rev()
 recovered_pal <- c("#1b5e20", "#388e3c", "#4caf50", "#81c784", "#c8e6c9") %>% rev()
-confirmed_pal <- c("#bf444c", "#d88273", "#f6efa6") %>%  rev()
+confirmed_pal <- c("#bf444c", "#d88273", "#f6efa6") %>% rev()
 suspected_pal <- c("#311b92", "#512da8", "#673ab7", "#9575cd", "#d1c4e9") %>% rev()
 
 # global variables
